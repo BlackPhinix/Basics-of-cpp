b5373801c20424c023e9c58d041f7eae62398433
Abena <72131115+BlackPhinix@users.noreply.github.com>
2021-06-03 20:40:52 +0300

    Add files via upload

---
+++ QN9A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ int main(){
+
+
+
+ int n = 4, k = 2;
+
+ 
+ cout << "++n = "<< ++n << endl;
+ cout << "n ="<< n << endl;
+ cout << "n++ =" << n++ << endl;
+ cout << "n =" << n <<endl;
+ cout << "++n =" << ++n << endl;
+ cout << "--n =" << --n << endl;
+ cout << "n ="<< n << endl;
+ cout << "n-- ="<< n-- << endl;
+ cout << "n ="<< n << endl;
+ cout << "n + k ="<< n + k << endl;
+ cout << " n * n = "<< endl; 
+ cout << "n * n = "<< n * n << endl;
+ cout << ' n '<< endl; 
+
+}
+
+
