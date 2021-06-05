e9176602975a1b74e7e46fdf79b92a6999109e42
Abena <72131115+BlackPhinix@users.noreply.github.com>
2021-06-05 20:27:23 +0300

    Add files via upload

---
+++ QN9C.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+			int n = 10;
+			while (n >0)
+			 cout << "(n /= 2) ="<< (n /= 2)<< endl;
+			 cout << "n * n ="<< n * n<< endl;
+			 	 
+			
+}
+
