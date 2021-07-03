1b7a99c2c5108b3993ba42654fa7169cf1bf1b0b
Abena <72131115+BlackPhinix@users.noreply.github.com>
2021-07-03 20:30:10 +0300

    Update fibanocci.cpp

--- fibanocci.cpp
+++ fibanocci.cpp
@@ -1,19 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int fib (int n){ 
-   if (n<=1) { 
-      return fib(n-1)+fib(n-2);
-   } 
-    else {
-   return 1;
-      }
+int fib(int n) {
+  if (n <= 1)
+    return (n);
+
+  else {
+    return ((n - 1) + fib(n - 2));
   }
-int main()
-{
-int num;
-cout<<"Enter fib number #\n";
-cin>>num;
-cout<<fib(num);
-return 0;
+}
+int main() {
+  int num;
+  cout << "Enter fib number #\n";
+  cin >> num;
+  cout << (fib(num));
+  return 0;
 }
