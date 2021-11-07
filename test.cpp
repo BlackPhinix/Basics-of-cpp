b29d3f2420f73ca16769c3443401f9538880648b
Abena <72131115+BlackPhinix@users.noreply.github.com>
2021-11-07 13:47:01 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+// write a program that displays the following pattern:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// Output:  
